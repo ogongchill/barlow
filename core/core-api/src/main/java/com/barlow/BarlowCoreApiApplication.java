19740eeefe109f905da876582478dd370f184d23
chobusiness <nation.normalization@gmail.com>
2024-12-08 19:04:51 +0900

    initial commit

---
+++ core/core-api/src/main/java/com/barlow/BarlowCoreApiApplication.java
@@ -0,0 +1,12 @@
+package com.barlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarlowCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarlowCoreApiApplication.class, args);
+	}
+}
