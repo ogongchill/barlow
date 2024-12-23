a788f7119f441e70d0c60f54c513e12fe30aca21
chobusiness <nation.normalization@gmail.com>
2024-12-23 16:23:52 +0900

    feature : notification async 스레드풀 설정

---
+++ core/core-notification/src/main/java/com/barlow/notification/config/AsyncConfiguration.java
@@ -0,0 +1,26 @@
+package com.barlow.notification.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfiguration {
+
+	@Bean(name = "asyncThreadPoolExecutor")
+	public Executor executor() {
+		ThreadPoolTaskExecutor threadPoolTask = new ThreadPoolTaskExecutor();
+		threadPoolTask.setCorePoolSize(10);
+		threadPoolTask.setMaxPoolSize(50);
+		threadPoolTask.setQueueCapacity(10);
+		threadPoolTask.setWaitForTasksToCompleteOnShutdown(true);
+		threadPoolTask.setAwaitTerminationSeconds(10);
+		threadPoolTask.setThreadNamePrefix("async-");
+		threadPoolTask.initialize();
+		return threadPoolTask;
+	}
+}
