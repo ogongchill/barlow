607555b1496bd7d46c167d5cdd98904cb413968d
chobusiness <nation.normalization@gmail.com>
2024-12-23 16:40:49 +0900

    feature : 알림 정보 reader 구현체

---
+++ core/core-notification/src/main/java/com/barlow/notification/DefaultNotificationInfoReader.java
@@ -0,0 +1,22 @@
+package com.barlow.notification;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DefaultNotificationInfoReader implements NotificationInfoReader {
+
+	private final NotificationInfoRepository notificationInfoRepository;
+
+	public DefaultNotificationInfoReader(NotificationInfoRepository notificationInfoRepository) {
+		this.notificationInfoRepository = notificationInfoRepository;
+	}
+
+	@Override
+	public List<NotificationInfo> readNotificationInfos(BillNotificationPayload payload) {
+		List<NotificationInfo> notificationInfos = notificationInfoRepository.retrieveNotificationInfosByTopic(payload.topic());
+		notificationInfos.forEach(info -> info.setTopicCount(payload.totalCount()));
+		return notificationInfos;
+	}
+}
