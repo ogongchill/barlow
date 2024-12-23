11273858179bf90a67255fbaa4d946d6f700934f
chobusiness <nation.normalization@gmail.com>
2024-12-23 23:53:55 +0900

    feature : 알림 전송 성공/실패 여부와 관계없이 사용자 알림센터에 저장

--- core/core-notification/src/main/java/com/barlow/notification/worker/NotificationSendWorker.java
+++ core/core-notification/src/main/java/com/barlow/notification/worker/NotificationSendWorker.java
@@ -1,12 +1,14 @@
 package com.barlow.notification.worker;
 
-import static com.barlow.notification.NotificationInfo.SubscriberDevice;
+import static com.barlow.notification.NotificationInfo.Subscriber;
 import static com.barlow.notification.NotificationInfo.Topic;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Component;
 
@@ -18,28 +20,56 @@ public class NotificationSendWorker {
 
 	private final IOSNotificationSender iosNotificationSender;
 	private final AndroidNotificationSender androidNotificationSender;
+	private final NotificationCenterRegistrar notificationCenterRegistrar;
 	private final Executor asyncThreadPoolExecutor;
 
 	public NotificationSendWorker(
 		IOSNotificationSender iosNotificationSender,
 		AndroidNotificationSender androidNotificationSender,
+		NotificationCenterRegistrar notificationCenterRegistrar,
 		Executor asyncThreadPoolExecutor
 	) {
 		this.iosNotificationSender = iosNotificationSender;
 		this.androidNotificationSender = androidNotificationSender;
+		this.notificationCenterRegistrar = notificationCenterRegistrar;
 		this.asyncThreadPoolExecutor = asyncThreadPoolExecutor;
 	}
 
-	public void invoke(MessageTemplate messageTemplate, Map<Topic, List<SubscriberDevice>> topicsWithSubscribers) {
-		CompletableFuture.runAsync(() -> {
-				List<Message> iosMessages = MessageProvider.generateIosMessages(messageTemplate, topicsWithSubscribers);
-				iosNotificationSender.send(iosMessages);
-			}, asyncThreadPoolExecutor
+	public void invoke(MessageTemplate messageTemplate, Map<Topic, List<Subscriber>> topicsWithSubscribers) {
+		CompletableFuture.runAsync(
+			billNotificationTask(messageTemplate, topicsWithSubscribers, Subscriber::isIOS,
+				new IosMessageProvider(), iosNotificationSender
+			), asyncThreadPoolExecutor
 		);
-		CompletableFuture.runAsync(() -> {
-				List<Message> androidMessages = MessageProvider.generateAndroidMessages(messageTemplate, topicsWithSubscribers);
-				androidNotificationSender.send(androidMessages);
-			}, asyncThreadPoolExecutor
+		CompletableFuture.runAsync(
+			billNotificationTask(messageTemplate, topicsWithSubscribers, Subscriber::isANDROID,
+				new AndroidMessageProvider(), androidNotificationSender
+			), asyncThreadPoolExecutor
 		);
+	}
+
+	private Runnable billNotificationTask(
+		MessageTemplate messageTemplate,
+		Map<Topic, List<Subscriber>> topicsWithSubscribers,
+		Predicate<Subscriber> subscriberOsPredicate,
+		MessageProvider messageProvider,
+		NotificationSender notificationSender
+	) {
+		List<Message> messages = new ArrayList<>();
+		List<NotificationCenterInfo> notificationCenterInfos = new ArrayList<>();
+		return () -> {
+			topicsWithSubscribers.forEach((topic, subscribers) -> {
+				String title = messageTemplate.getMessageTitleFormat(topic);
+				String body = messageTemplate.getMessageBodyFormat(topic);
+				subscribers.stream()
+					.filter(subscriberOsPredicate)
+					.forEach(subscriber -> {
+						notificationCenterInfos.add(new NotificationCenterInfo(subscriber, topic, title, body));
+						messages.add(messageProvider.provide(title, body, subscriber));
+					});
+			});
+			notificationCenterRegistrar.registerAll(notificationCenterInfos);
+			notificationSender.send(messages);
+		};
 	}
 }
