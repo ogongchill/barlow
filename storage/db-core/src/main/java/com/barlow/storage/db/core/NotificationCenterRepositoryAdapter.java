2eb1d3a983d7246b038c9a7a4cdeabde7f5cb72f
chobusiness <nation.normalization@gmail.com>
2024-12-26 19:21:05 +0900

    fix : enum 데이터 삽입 시 데이터 변환 오류 해결

--- storage/db-core/src/main/java/com/barlow/storage/db/core/NotificationCenterRepositoryAdapter.java
+++ storage/db-core/src/main/java/com/barlow/storage/db/core/NotificationCenterRepositoryAdapter.java
@@ -37,7 +37,7 @@ public class NotificationCenterRepositoryAdapter implements NotificationCenterRe
 		NotificationTopic topic = NotificationTopic.valueOf(notificationCenterInfo.topic());
 		return new MapSqlParameterSource()
 			.addValue("member_no", notificationCenterInfo.memberNo())
-			.addValue("notification_topic", topic)
+			.addValue("notification_topic", topic.name())
 			.addValue("icon_url", topic.getIconUrl())
 			.addValue("title", notificationCenterInfo.title())
 			.addValue("body", notificationCenterInfo.body())
