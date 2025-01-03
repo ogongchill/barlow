b84ba7b26c8e7213bbf7f27a4905f2d36d18b7a7
chobusiness <nation.normalization@gmail.com>
2025-01-04 00:12:10 +0900

    feature : 최근 법안 게시글 thumbnail 조회 controller

---
+++ core/core-api/src/main/java/com/barlow/core/api/controller/v1/recentbill/RecentBillRetrieveController.java
@@ -0,0 +1,39 @@
+package com.barlow.core.api.controller.v1.recentbill;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.barlow.core.domain.recentbill.BillPostQuery;
+import com.barlow.core.domain.recentbill.RecentBillPostRetrieveService;
+import com.barlow.core.domain.recentbill.RecentBillPostsStatus;
+import com.barlow.core.support.response.ApiResponse;
+
+@RestController
+@RequestMapping("/api/v1/recent-bill")
+public class RecentBillRetrieveController {
+
+	private final RecentBillPostRetrieveService recentBillPostRetrieveService;
+
+	public RecentBillRetrieveController(RecentBillPostRetrieveService recentBillPostRetrieveService) {
+		this.recentBillPostRetrieveService = recentBillPostRetrieveService;
+	}
+
+	@GetMapping("/thumbnail")
+	public ApiResponse<RecentBillPostsResponse> retrieveRecentBill(
+		@RequestParam(name = "page") Integer page,
+		@RequestParam(name = "size") Integer size,
+		@RequestParam(name = "sort", required = false) String sortKey,
+		@RequestParam(required = false) Map<String, List<String>> tags
+	) {
+		RecentBillPostsStatus recentBillPostsStatus
+			= recentBillPostRetrieveService.readRecentBillPosts(new BillPostQuery(page, size, sortKey, tags));
+		RecentBillPostsApiSpecComposer apiSpecComposer = new RecentBillPostsApiSpecComposer(recentBillPostsStatus);
+		return ApiResponse.success(apiSpecComposer.compose(LocalDate.now()));
+	}
+}
