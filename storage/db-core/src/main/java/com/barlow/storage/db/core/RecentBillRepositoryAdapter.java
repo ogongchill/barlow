54cce4cb77c8a2d7e9e91eae219215c0ce181391
chobusiness <nation.normalization@gmail.com>
2025-01-03 23:49:19 +0900

    feature : 최근 법안 게시글 repository

---
+++ storage/db-core/src/main/java/com/barlow/storage/db/core/RecentBillRepositoryAdapter.java
@@ -0,0 +1,51 @@
+package com.barlow.storage.db.core;
+
+import java.util.List;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Component;
+
+import com.barlow.core.domain.recentbill.BillPostQuery;
+import com.barlow.core.domain.recentbill.RecentBillPost;
+import com.barlow.core.domain.recentbill.RecentBillPostRepository;
+import com.barlow.core.domain.recentbill.RecentBillPostsStatus;
+
+@Component
+public class RecentBillRepositoryAdapter implements RecentBillPostRepository {
+
+	private final RecentBillPostJpaRepository recentBillPostJpaRepository;
+
+	public RecentBillRepositoryAdapter(RecentBillPostJpaRepository recentBillPostJpaRepository) {
+		this.recentBillPostJpaRepository = recentBillPostJpaRepository;
+	}
+
+	@Override
+	public RecentBillPostsStatus retrieveRecentBillPosts(BillPostQuery query) {
+		SortKey sortKey = new SortKey(query.sortKey());
+		BillPostFilterTag filterTag = BillPostFilterTag.from(query.tags());
+		Pageable pageable = PageRequest.of(query.page(), query.size(), sortKey.getSort());
+		Slice<RecentBillPostJpaEntity> billPostJpaEntities;
+		if (filterTag.isPartyNameTagEmpty()) {
+			billPostJpaEntities = recentBillPostJpaRepository.findAllBy(
+				filterTag.getLegislationTypeTags(),
+				filterTag.getLegislationStatusTags(),
+				filterTag.getProposerTypeTags(),
+				pageable
+			);
+		} else {
+			billPostJpaEntities = recentBillPostJpaRepository.findAllBy(
+				filterTag.getLegislationTypeTags(),
+				filterTag.getLegislationStatusTags(),
+				filterTag.getProposerTypeTags(),
+				filterTag.getPartyNameTags(),
+				pageable
+			);
+		}
+		List<RecentBillPost> recentBillPosts = billPostJpaEntities.stream()
+			.map(RecentBillPostJpaEntity::toRecentBillPost)
+			.toList();
+		return new RecentBillPostsStatus(recentBillPosts, billPostJpaEntities.isLast());
+	}
+}
