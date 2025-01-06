427f338aaab31bbf973350e624fa89cd4bb9b6f8
chobusiness <nation.normalization@gmail.com>
2025-01-07 00:36:07 +0900

    add : PartyName enum 필드 추가

--- storage/db-core/src/main/java/com/barlow/storage/db/core/PartyName.java
+++ storage/db-core/src/main/java/com/barlow/storage/db/core/PartyName.java
@@ -1,4 +1,16 @@
 package com.barlow.storage.db.core;
 
-public enum PartyName { // 정당명
+public enum PartyName {
+
+	; // 정당명
+
+	private final String value;
+
+	PartyName(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
 }
