8f77ddd37b7f3f0d37bfee5ce0b04edb25449d14
chobusiness <nation.normalization@gmail.com>
2025-01-07 00:35:07 +0900

    add : 법안발의자 jpa entity 필드 추가

--- storage/db-core/src/main/java/com/barlow/storage/db/core/BillProposerJpaEntity.java
+++ storage/db-core/src/main/java/com/barlow/storage/db/core/BillProposerJpaEntity.java
@@ -1,5 +1,7 @@
 package com.barlow.storage.db.core;
 
+import com.barlow.core.domain.recentbill.BillProposer;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -21,13 +23,28 @@ public class BillProposerJpaEntity extends BaseTimeJpaEntity {
 	@Column(name = "propose_bill_id", nullable = false)
 	private String proposeBillId;
 
-	@Column(name = "lawmaker_no", nullable = false)
-	private Long lawmakerNo;
+	@Column(name = "proposer_no", nullable = false)
+	private Long proposerNo;
+
+	@Column(name = "proposer_name", nullable = false)
+	private String proposerName;
+
+	@Column(name = "proposer_profile_image_path", nullable = false)
+	private String proposerProfileImagePath;
 
 	@Enumerated(EnumType.STRING)
 	@Column(name = "party_name", nullable = false)
 	private PartyName partyName;
 
 	protected BillProposerJpaEntity() {
+	}
+
+	BillProposer toBillProposer() {
+		return new BillProposer(
+			proposerNo,
+			proposerName,
+			partyName.getValue(),
+			proposerProfileImagePath
+		);
 	}
 }
