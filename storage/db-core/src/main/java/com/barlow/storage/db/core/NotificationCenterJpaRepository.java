15c3913d911aa1af73030269c79e1459eaaadf43
chobusiness <nation.normalization@gmail.com>
2025-01-11 17:55:12 +0900

    add : 사용자에게 오늘 도착한 알림 존재 여부 확인 쿼리

--- storage/db-core/src/main/java/com/barlow/storage/db/core/NotificationCenterJpaRepository.java
+++ storage/db-core/src/main/java/com/barlow/storage/db/core/NotificationCenterJpaRepository.java
@@ -1,6 +1,10 @@
 package com.barlow.storage.db.core;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationCenterJpaRepository extends JpaRepository<NotificationCenterJpaEntity, Long> {
+
+	boolean existsByCreatedAtBetweenAndMemberNo(LocalDateTime startDateTime, LocalDateTime endDateTime, Long memberNo);
 }
