f50e7f1bb111199c39aa9c964dc98c6d906ad191
chobusiness <nation.normalization@gmail.com>
2025-01-14 17:49:37 +0900

    chore : 주석 제거

--- core/core-domain/src/main/java/com/barlow/core/domain/home/notificationcenter/NotificationCenterItem.java
+++ core/core-domain/src/main/java/com/barlow/core/domain/home/notificationcenter/NotificationCenterItem.java
@@ -2,9 +2,6 @@ package com.barlow.core.domain.home.notificationcenter;
 
 import java.time.LocalDateTime;
 
-/**
- * @param iconImagePath todo : 2025.01.11 - 알림센터 jpa entity 에서 제거
- */
 public record NotificationCenterItem(
 	String billId,
 	String notificationTopic,
