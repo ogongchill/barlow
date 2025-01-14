964c7383807a88dec9d679398d33eae0b9f6f737
chobusiness <nation.normalization@gmail.com>
2025-01-14 17:45:32 +0900

    fix : JpaRepository 엔티티 타입 오류 수정

--- storage/db-core/src/main/java/com/barlow/storage/db/core/BillProposerJpaRepository.java
+++ storage/db-core/src/main/java/com/barlow/storage/db/core/BillProposerJpaRepository.java
@@ -6,9 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.barlow.core.domain.recentbill.BillProposer;
-
-public interface BillProposerJpaRepository extends JpaRepository<BillProposer, Long> {
+public interface BillProposerJpaRepository extends JpaRepository<BillProposerJpaEntity, Long> {
 
 	@Query("SELECT bp FROM BillProposerJpaEntity bp WHERE bp.proposeBillId = :billId")
 	List<BillProposerJpaEntity> findAllByBillId(@Param("billId") String billId);
