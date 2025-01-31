e2f4662457abc8f7a47fcb928b50143b8b4f98fe
Sy P <yongsa0221@gmail.com>
2025-01-31 23:55:21 +0900

    feature: RSA키 생성 기능 추가

---
+++ core/core-auth/src/main/java/com/barlow/core/auth/crypto/PrivateKeyAlgorithm.java
@@ -0,0 +1,18 @@
+package com.barlow.core.auth.crypto;
+
+import com.auth0.jwt.algorithms.Algorithm;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.InvalidKeySpecException;
+
+public class PrivateKeyAlgorithm extends KeyAlgorithm {
+
+    protected PrivateKeyAlgorithm(Algorithm algorithm) {
+        super(algorithm);
+    }
+
+    public static PrivateKeyAlgorithm from(String keyString) throws InvalidKeySpecException {
+        RSAPrivateKey privateKey = RSAKeyFactory.INSTANCE.createPrivateKey(keyString);
+        return new PrivateKeyAlgorithm(Algorithm.RSA256(privateKey));
+    }
+}
---
+++ core/core-auth/src/main/java/com/barlow/core/auth/crypto/RSAKeyFactory.java
@@ -0,0 +1,49 @@
+package com.barlow.core.auth.crypto;
+
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+public enum RSAKeyFactory {
+
+    INSTANCE;
+
+    private static final String PKCS8_HEADER = "-----BEGIN PRIVATE KEY-----";
+    private static final String PKCS8_FOOTER = "-----END PRIVATE KEY-----";
+    private static final String X509_HEADER = "-----BEGIN PUBLIC KEY-----";
+    private static final String X509_FOOTER = "-----END PUBLIC KEY-----";
+    private static final String SPACE = "\\s";
+    private final KeyFactory keyFactory;
+
+
+    RSAKeyFactory() {
+        try {
+            keyFactory = KeyFactory.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("RSAKeyFactory initializing failed");
+        }
+    }
+
+    public RSAPublicKey createPublicKey(String keyString) throws InvalidKeySpecException {
+        String refinedKey = keyString.replace(X509_HEADER, "")
+                .replace(X509_FOOTER, "")
+                .replaceAll(SPACE, "");
+        byte[] decoded = Base64.getDecoder().decode(refinedKey);
+        X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(decoded);
+        return (RSAPublicKey) keyFactory.generatePublic(publicKeySpec);
+    }
+
+    public RSAPrivateKey createPrivateKey(String keyString) throws InvalidKeySpecException {
+        String refinedKey = keyString.replace(PKCS8_HEADER, "")
+                .replace(PKCS8_FOOTER, "")
+                .replaceAll(SPACE, "");
+        byte[] decoded = Base64.getDecoder().decode(refinedKey);
+        PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(decoded);
+        return (RSAPrivateKey) keyFactory.generatePrivate(privateKeySpec);
+    }
+}
