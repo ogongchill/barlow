53536e98adf4add85fad8e5e9c9b66496a1dc942
chobusiness <nation.normalization@gmail.com>
2025-02-09 17:21:16 +0900

    refactor : db-core 용 emf, txm 수동 설정

--- storage/db-core/src/main/java/com/barlow/storage/db/core/config/CoreJpaConfig.java
+++ storage/db-core/src/main/java/com/barlow/storage/db/core/config/CoreJpaConfig.java
@@ -4,10 +4,12 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -16,17 +18,27 @@ import jakarta.persistence.EntityManagerFactory;
 @Configuration
 @EnableTransactionManagement
 @EntityScan(basePackages = "com.barlow.storage.db.core")
-@EnableJpaRepositories(basePackages = "com.barlow.storage.db.core")
+@EnableJpaRepositories(
+	basePackages = "com.barlow.storage.db.core",
+	entityManagerFactoryRef = "coreEntityManagerFactory",
+	transactionManagerRef = "coreTransactionManager")
 public class CoreJpaConfig {
 
 	@Bean("coreTransactionManager")
 	public PlatformTransactionManager platformTransactionManager(
-		EntityManagerFactory emf,
-		@Qualifier("coreDataSource") DataSource dataSource
+		@Qualifier("coreEntityManagerFactory") EntityManagerFactory emf
 	) {
-		JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
-		jpaTransactionManager.setEntityManagerFactory(emf);
-		jpaTransactionManager.setDataSource(dataSource);
-		return jpaTransactionManager;
+		return new JpaTransactionManager(emf);
+	}
+
+	@Bean("coreEntityManagerFactory")
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+		@Qualifier("coreDataSource") DataSource dataSource,
+		EntityManagerFactoryBuilder builder
+	) {
+		return builder.dataSource(dataSource)
+			.packages("com.barlow.storage.db.core")
+			.persistenceUnit("core")
+			.build();
 	}
 }
