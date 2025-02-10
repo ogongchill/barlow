64d87e4263098597c3c217e319cfb2410bd7007b
chobusiness <nation.normalization@gmail.com>
2025-02-10 14:54:24 +0900

    feature : job 에서 공유하는 자원을 관리하는 job execution listener

---
+++ batch/batch-core/src/main/java/com/barlow/batch/core/recentbill/job/listener/RetrieveTodayBillJobListener.java
@@ -0,0 +1,43 @@
+package com.barlow.batch.core.recentbill.job.listener;
+
+import java.time.LocalDate;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.stereotype.Component;
+
+import com.barlow.batch.core.recentbill.job.NationalAssemblyLegislationClient;
+import com.barlow.batch.core.recentbill.job.TodayBillInfoResult;
+import com.barlow.batch.core.recentbill.job.AbstractExecutionContextSharingManager;
+import com.barlow.batch.core.recentbill.job.RecentBillJobScopeShareRepository;
+import com.barlow.batch.core.utils.HashUtil;
+
+@Component
+public class RetrieveTodayBillJobListener
+	extends AbstractExecutionContextSharingManager
+	implements JobExecutionListener {
+
+	private final NationalAssemblyLegislationClient client;
+	private final RecentBillJobScopeShareRepository jobScopeShareRepository;
+
+	public RetrieveTodayBillJobListener(
+		NationalAssemblyLegislationClient client,
+		RecentBillJobScopeShareRepository jobScopeShareRepository
+	) {
+		super();
+		this.client = client;
+		this.jobScopeShareRepository = jobScopeShareRepository;
+	}
+
+	@Override
+	public void beforeJob(@NotNull JobExecution jobExecution) {
+		TodayBillInfoResult todayBillInfo = client.getTodayBillInfo(LocalDate.now());
+
+		String hashKey = HashUtil.generate(todayBillInfo);
+		super.setCurrentExecutionContext(jobExecution.getExecutionContext());
+		super.putDataToExecutionContext(TODAY_BILL_INFO_JOB_KEY, hashKey);
+
+		jobScopeShareRepository.save(hashKey, todayBillInfo);
+	}
+}
