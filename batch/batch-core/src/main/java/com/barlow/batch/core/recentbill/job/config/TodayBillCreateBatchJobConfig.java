1dcf249452078cf06598d220f9528fc3055d3db0
chobusiness <nation.normalization@gmail.com>
2025-02-10 15:04:10 +0900

    refactor : 누락된 step 추가 및 job 완성

--- batch/batch-core/src/main/java/com/barlow/batch/core/recentbill/job/config/TodayBillCreateBatchJobConfig.java
+++ batch/batch-core/src/main/java/com/barlow/batch/core/recentbill/job/config/TodayBillCreateBatchJobConfig.java
@@ -1,6 +1,7 @@
 package com.barlow.batch.core.recentbill.job.config;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
@@ -17,6 +18,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 
+import com.barlow.batch.core.recentbill.job.listener.StepExecutionContextSharingListener;
+import com.barlow.batch.core.recentbill.job.listener.StepLoggingListener;
 import com.barlow.batch.core.recentbill.job.step.BillProposer;
 
 @Configuration
@@ -29,10 +32,12 @@ public class TodayBillCreateBatchJobConfig {
 	}
 
 	@Bean
-	public Job todayBillCreateBatchJob() {
+	public Job todayBillCreateBatchJob(JobExecutionListener jobExecutionListener) {
 		return new JobBuilder("todayBillCreateBatchJob", jobRepository)
-			.start(writeTodayBillInfoStep(null, null))
-			.next(setBillProposerStep(null, null, null, null))
+			.listener(jobExecutionListener)
+			.start(writeTodayBillInfoStep(null, null, null))
+			.next(setBillProposerStep(null, null, null, null, null, null))
+			.next(notifyTodayBillStep(null, null, null))
 			.build();
 	}
 
@@ -40,10 +45,12 @@ public class TodayBillCreateBatchJobConfig {
 	@JobScope
 	public Step writeTodayBillInfoStep(
 		@Qualifier("todayBillInfoWriteTasklet") Tasklet tasklet,
-		@Qualifier("coreTransactionManager") PlatformTransactionManager transactionManager
+		@Qualifier("coreTransactionManager") PlatformTransactionManager transactionManager,
+		StepLoggingListener stepLoggingListener
 	) {
 		return new StepBuilder("writeTodayBillInfoStep", jobRepository)
 			.tasklet(tasklet, transactionManager)
+			.listener(stepLoggingListener)
 			.build();
 	}
 
@@ -52,6 +59,8 @@ public class TodayBillCreateBatchJobConfig {
 	public Step setBillProposerStep(
 		@Value("#{jobParameters[chunkSize]}") Integer chunkSize,
 		@Qualifier("coreTransactionManager") PlatformTransactionManager transactionManager,
+		StepLoggingListener stepLoggingListener,
+		StepExecutionContextSharingListener stepExecutionContextSharingListener,
 		ItemReader<BillProposer> billProposerReader,
 		ItemWriter<BillProposer> billProposerWriter
 	) {
@@ -59,6 +68,8 @@ public class TodayBillCreateBatchJobConfig {
 			.<BillProposer, BillProposer>chunk(chunkSize, transactionManager)
 			.reader(billProposerReader)
 			.writer(billProposerWriter)
+			.listener(stepExecutionContextSharingListener)
+			.listener(stepLoggingListener)
 			.build();
 	}
 
@@ -66,13 +77,15 @@ public class TodayBillCreateBatchJobConfig {
 	@JobScope
 	public Step notifyTodayBillStep(
 		@Qualifier("todayBillNotifierTasklet") Tasklet tasklet,
-		@Qualifier("coreTransactionManager") PlatformTransactionManager transactionManager
+		@Qualifier("coreTransactionManager") PlatformTransactionManager transactionManager,
+		StepLoggingListener stepLoggingListener
 	) {
 		DefaultTransactionAttribute transactionAttribute = new DefaultTransactionAttribute();
 		transactionAttribute.setPropagationBehavior(TransactionDefinition.PROPAGATION_NEVER);
 		return new StepBuilder("todayBillNotifierTasklet", jobRepository)
 			.tasklet(tasklet, transactionManager)
 			.transactionAttribute(transactionAttribute)
+			.listener(stepLoggingListener)
 			.build();
 	}
 }
