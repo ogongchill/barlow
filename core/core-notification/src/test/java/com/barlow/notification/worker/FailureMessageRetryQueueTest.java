c1e81e3f5f26828453569a097a3e692dea87609a
chobuisness <nation.normalization@gmail.com>
2025-02-13 13:07:52 +0900

    Notification/test/issue/33 : 알림전송 worker, 재전송 worker, 실패 메시지 관리 큐 등 리팩토링 및 일부 테스트 (#34)
    * refactor : 실패 메시지 재시도 큐 리팩토링
    
    * refactor : 알림 전송 추상 객체 리팩토링
    
    * refactor : 알림 전송 worker 리팩토링
    
    * feature : 재시도 worker 개발
    
    * feature : 비동기 알림 전송 결과 관리 객체
    
    * test : 실패 메시지 재시도 큐 테스트
    
    * test : 비동기 알림 전송 결과 관리 객체 테스트

---
+++ core/core-notification/src/test/java/com/barlow/notification/worker/FailureMessageRetryQueueTest.java
@@ -0,0 +1,46 @@
+package com.barlow.notification.worker;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.google.firebase.messaging.Message;
+
+class FailureMessageRetryQueueTest {
+
+	private FailureMessageRetryQueue testRetryQueue;
+	private List<Message> retryableMessages;
+
+	@BeforeEach
+	void setUp() {
+		testRetryQueue = new FailureMessageRetryQueue(0, 1);
+		retryableMessages = List.of(
+			Message.builder().setToken("test").build(),
+			Message.builder().setToken("test").build(),
+			Message.builder().setToken("test").build()
+		);
+	}
+
+	@DisplayName("재시도 가능한 실패 메시지들을 queue 에 삽입한다")
+	@Test
+	void pushAllRetryableMessages() throws InterruptedException {
+		testRetryQueue.pushAllRetryableMessages(retryableMessages);
+
+		assertThat(testRetryQueue.isEmpty()).isFalse();
+		assertThat(testRetryQueue.take()).isInstanceOf(FailureMessageRetryQueue.RetryBatchMessages.class);
+	}
+
+	@DisplayName("실패 메시지 재시도 queue 를 비운다")
+	@Test
+	void clear() {
+		testRetryQueue.pushAllRetryableMessages(retryableMessages);
+
+		testRetryQueue.clear();
+
+		assertThat(testRetryQueue.isEmpty()).isTrue();
+	}
+}
