b0d2e3a5d9c83f14a2a2df211aedc9f223ac0cb7
chobusiness <nation.normalization@gmail.com>
2025-02-14 23:44:11 +0900

    feature : 알림 설정 domain exception

---
+++ core/core-domain/src/main/java/com/barlow/core/domain/account/notificationsetting/NotificationSettingDomainException.java
@@ -0,0 +1,24 @@
+package com.barlow.core.domain.account.notificationsetting;
+
+import com.barlow.core.exception.CoreDomainException;
+import com.barlow.core.exception.CoreDomainExceptionCode;
+import com.barlow.core.exception.CoreDomainExceptionLevel;
+import com.barlow.core.exception.CoreDomainExceptionType;
+
+public class NotificationSettingDomainException extends CoreDomainException {
+
+	NotificationSettingDomainException(CoreDomainExceptionCode code, CoreDomainExceptionLevel level, String message) {
+		super(code, level, message);
+	}
+
+	NotificationSettingDomainException(CoreDomainExceptionType exceptionType, String message) {
+		super(exceptionType, message);
+	}
+
+	static NotificationSettingDomainException alreadyRegistered(String committeeName) {
+		return new NotificationSettingDomainException(
+			CoreDomainExceptionType.CONFLICT_EXCEPTION,
+			String.format("이미 알림 설정 되어있는 %s 입니다", committeeName)
+		);
+	}
+}
