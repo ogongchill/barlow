a67d217c13eed310f3745f3aa29368ac11233061
chobusiness <nation.normalization@gmail.com>
2025-02-16 23:40:03 +0900

    add : 구독자 증가/감소 로직 추가

--- storage/db-core/src/main/java/com/barlow/storage/db/core/LegislationRepositoryAdapter.java
+++ storage/db-core/src/main/java/com/barlow/storage/db/core/LegislationRepositoryAdapter.java
@@ -2,6 +2,7 @@ package com.barlow.storage.db.core;
 
 import com.barlow.core.domain.legislationaccount.LegislationAccount;
 import com.barlow.core.domain.legislationaccount.LegislationAccountRepository;
+
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -9,18 +10,28 @@ import java.util.List;
 @Component
 public class LegislationRepositoryAdapter implements LegislationAccountRepository {
 
-    private final LegislationAccountJpaRepository legislationAccountJpaRepository;
+	private final LegislationAccountJpaRepository legislationAccountJpaRepository;
 
-    public LegislationRepositoryAdapter(LegislationAccountJpaRepository legislationAccountJpaRepository) {
-        this.legislationAccountJpaRepository = legislationAccountJpaRepository;
-    }
+	public LegislationRepositoryAdapter(LegislationAccountJpaRepository legislationAccountJpaRepository) {
+		this.legislationAccountJpaRepository = legislationAccountJpaRepository;
+	}
 
-    @Override
-    public List<LegislationAccount> retrieveCommitteeAccount() {
-        return legislationAccountJpaRepository.findAll()
-                .stream()
-                .filter(LegislationAccountJpaEntity::isCommittee)
-                .map(LegislationAccountJpaEntity::toLegislationAccount)
-                .toList();
-    }
+	@Override
+	public List<LegislationAccount> retrieveCommitteeAccount() {
+		return legislationAccountJpaRepository.findAll()
+			.stream()
+			.filter(LegislationAccountJpaEntity::isCommittee)
+			.map(LegislationAccountJpaEntity::toLegislationAccount)
+			.toList();
+	}
+
+	@Override
+	public void incrementSubscriber(long accountNo) {
+		legislationAccountJpaRepository.updateIncrementSubscriberCount(accountNo);
+	}
+
+	@Override
+	public void decrementSubscriber(long accountNo) {
+		legislationAccountJpaRepository.updateDecrementSubscriberCount(accountNo);
+	}
 }
