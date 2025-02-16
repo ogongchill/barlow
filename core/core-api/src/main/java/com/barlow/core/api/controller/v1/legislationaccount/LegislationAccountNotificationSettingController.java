af2ad304543eaa497f6cad2a40118e7a334e7cbe
chobusiness <nation.normalization@gmail.com>
2025-02-17 00:30:12 +0900

    feature : 입법계정 알림설정 controller

---
+++ core/core-api/src/main/java/com/barlow/core/api/controller/v1/legislationaccount/LegislationAccountNotificationSettingController.java
@@ -0,0 +1,33 @@
+package com.barlow.core.api.controller.v1.legislationaccount;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.barlow.core.domain.User;
+import com.barlow.core.domain.notificationsetting.NotificationSettingService;
+import com.barlow.core.support.response.ApiResponse;
+
+@RestController
+@RequestMapping("/api/v1/legislation-account/notification-setting")
+public class LegislationAccountNotificationSettingController {
+
+	private final NotificationSettingService notificationSettingService;
+
+	public LegislationAccountNotificationSettingController(NotificationSettingService notificationSettingService) {
+		this.notificationSettingService = notificationSettingService;
+	}
+
+	@PostMapping("/{committeeName}")
+	public ApiResponse<Void> subscribe(@PathVariable String committeeName, User user) {
+		notificationSettingService.activateSetting(committeeName, user);
+		return ApiResponse.success();
+	}
+
+	@PostMapping("/{committeeName}")
+	public ApiResponse<Void> unsubscribe(@PathVariable String committeeName, User user) {
+		notificationSettingService.deactivateSetting(committeeName, user);
+		return ApiResponse.success();
+	}
+}
