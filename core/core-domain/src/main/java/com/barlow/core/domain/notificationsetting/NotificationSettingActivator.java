3bf9acd3a64d4334da7a9ab5ad2e563d30c22da6
chobusiness <nation.normalization@gmail.com>
2025-02-16 23:47:22 +0900

    refactor : 알림설정 활성화/비활성화 implementation 컴포넌트 리팩토링

--- core/core-domain/src/main/java/com/barlow/core/domain/notificationsetting/NotificationSettingActivator.java
+++ core/core-domain/src/main/java/com/barlow/core/domain/notificationsetting/NotificationSettingActivator.java
@@ -2,20 +2,35 @@ package com.barlow.core.domain.notificationsetting;
 
 import org.springframework.stereotype.Component;
 
+import com.barlow.core.domain.User;
+
 @Component
 public class NotificationSettingActivator {
 
+	private final NotificationSettingReader notificationSettingReader;
 	private final NotificationSettingRepository notificationSettingRepository;
 
-	public NotificationSettingActivator(NotificationSettingRepository notificationSettingRepository) {
+	public NotificationSettingActivator(
+		NotificationSettingReader notificationSettingReader,
+		NotificationSettingRepository notificationSettingRepository
+	) {
+		this.notificationSettingReader = notificationSettingReader;
 		this.notificationSettingRepository = notificationSettingRepository;
 	}
 
-	public void activate(NotificationSetting notificationSetting) {
-		notificationSettingRepository.saveNotificationSetting(notificationSetting.activate());
+	public void activate(String committeeName, User user) {
+		NotificationSetting notificationSetting = notificationSettingReader.readNotificationSetting(committeeName, user);
+		if (notificationSetting.isNotifiable()) {
+			throw NotificationSettingDomainException.alreadyRegistered(committeeName);
+		}
+		notificationSettingRepository.saveNotificationSetting(notificationSetting);
 	}
 
-	public void deactivate(NotificationSetting notificationSetting) {
-		notificationSettingRepository.deleteNotificationSetting(notificationSetting.deactivate());
+	public void deactivate(String committeeName, User user) {
+		NotificationSetting notificationSetting = notificationSettingReader.readNotificationSetting(committeeName, user);
+		if (!notificationSetting.isNotifiable()) {
+			throw NotificationSettingDomainException.alreadyRegistered(committeeName);
+		}
+		notificationSettingRepository.deleteNotificationSetting(notificationSetting);
 	}
 }
--- core/core-domain/src/main/java/com/barlow/core/domain/notificationsetting/NotificationSettingService.java
+++ core/core-domain/src/main/java/com/barlow/core/domain/notificationsetting/NotificationSettingService.java
@@ -7,30 +7,17 @@ import com.barlow.core.domain.User;
 @Service
 public class NotificationSettingService {
 
-	private final NotificationSettingReader notificationSettingReader;
 	private final NotificationSettingActivator notificationSettingActivator;
 
-	public NotificationSettingService(
-		NotificationSettingReader notificationSettingReader,
-		NotificationSettingActivator notificationSettingActivator
-	) {
-		this.notificationSettingReader = notificationSettingReader;
+	public NotificationSettingService(NotificationSettingActivator notificationSettingActivator) {
 		this.notificationSettingActivator = notificationSettingActivator;
 	}
 
 	public void activateSetting(String committeeName, User user) {
-		NotificationSetting notificationSetting = notificationSettingReader.readNotificationSetting(committeeName, user);
-		if (notificationSetting.isNotifiable()) {
-			throw NotificationSettingDomainException.alreadyRegistered(committeeName);
-		}
-		notificationSettingActivator.activate(notificationSetting);
+		notificationSettingActivator.activate(committeeName, user);
 	}
 
 	public void deactivateSetting(String committeeName, User user) {
-		NotificationSetting notificationSetting = notificationSettingReader.readNotificationSetting(committeeName, user);
-		if (!notificationSetting.isNotifiable()) {
-			throw NotificationSettingDomainException.alreadyRegistered(committeeName);
-		}
-		notificationSettingActivator.deactivate(notificationSetting);
+		notificationSettingActivator.deactivate(committeeName, user);
 	}
 }
