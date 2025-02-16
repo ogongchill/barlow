dd96ad177038646cf2165cac25f3347345d70820
chobusiness <nation.normalization@gmail.com>
2025-02-16 23:28:18 +0900

    feature : 구독 도메인 엔티티

---
+++ core/core-domain/src/main/java/com/barlow/core/domain/subscribe/Subscribe.java
@@ -0,0 +1,45 @@
+package com.barlow.core.domain.subscribe;
+
+import com.barlow.core.domain.User;
+
+public class Subscribe {
+
+	private final User subscriber;
+	private final SubscriptionInfo subscriptionInfo;
+
+	public Subscribe(User subscriber, long legislationAccountNo, boolean isSubscribed) {
+		this.subscriber = subscriber;
+		this.subscriptionInfo = new SubscriptionInfo(legislationAccountNo, isSubscribed);
+	}
+
+	Subscribe activate() {
+		return new Subscribe(subscriber, subscriptionInfo.legislationAccountNo, true);
+	}
+
+	Subscribe deactivate() {
+		return new Subscribe(subscriber, subscriptionInfo.legislationAccountNo, false);
+	}
+
+	boolean isActive() {
+		return subscriptionInfo.isSubscribed;
+	}
+
+	public User getSubscriber() {
+		return subscriber;
+	}
+
+	public long getLegislationAccountNo() {
+		return subscriptionInfo.legislationAccountNo;
+	}
+
+	static class SubscriptionInfo {
+
+		private final long legislationAccountNo;
+		private final boolean isSubscribed;
+
+		public SubscriptionInfo(long legislationAccountNo, boolean isSubscribed) {
+			this.legislationAccountNo = legislationAccountNo;
+			this.isSubscribed = isSubscribed;
+		}
+	}
+}
