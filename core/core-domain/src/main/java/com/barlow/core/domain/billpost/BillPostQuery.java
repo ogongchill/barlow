512f4961979486e0a9a6ff41a350a3205f8ba2e4
chobusiness <nation.normalization@gmail.com>
2025-02-18 01:11:33 +0900

    add : 입법계정 별 법안 게시글 검색용 객체를 생성하는 정적 메서드 추가

--- core/core-domain/src/main/java/com/barlow/core/domain/billpost/BillPostQuery.java
+++ core/core-domain/src/main/java/com/barlow/core/domain/billpost/BillPostQuery.java
@@ -11,4 +11,29 @@ public record BillPostQuery(
 	@NotNull String sortKey,
 	@NotNull Map<String, List<String>> tags
 ) {
+	public static BillPostQuery legislationOf(
+		String legislationType,
+		Integer page,
+		Integer size,
+		String sortKey,
+		Map<String, List<String>> tags
+	) {
+		String legislationTypeKey = "legislationType";
+		String progressStatusKey = "progressStatus";
+		tags.putAll(Map.of(
+			legislationTypeKey, List.of(legislationType),
+			progressStatusKey, List.of(
+				"COMMITTEE_RECEIVED",
+				"COMMITTEE_REVIEW",
+				"PLENARY_SUBMITTED",
+				"PLENARY_DECIDED",
+				"WITHDRAWN",
+				"GOVERNMENT_TRANSFERRED",
+				"REDEMAND_REQUESTED",
+				"REJECTED",
+				"PROMULGATED"
+			)
+		));
+		return new BillPostQuery(page, size, sortKey, tags);
+	}
 }
