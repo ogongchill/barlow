83068a233cfb43c631be5da8488df29230ba0d38
chobusiness <nation.normalization@gmail.com>
2025-02-21 23:44:12 +0900

    add : ProgressStatus 필드 추가

--- core/core-domain/src/main/java/com/barlow/core/enumerate/ProgressStatus.java
+++ core/core-domain/src/main/java/com/barlow/core/enumerate/ProgressStatus.java
@@ -1,11 +1,15 @@
 package com.barlow.core.enumerate;
 
+import java.util.Arrays;
+
 public enum ProgressStatus {
 
 	RECEIVED("접수"),
 	COMMITTEE_RECEIVED("소관위접수"),
 	COMMITTEE_REVIEW("소관위심사"),
-	PLENARY_SUBMITTED("본회의부의"),
+	REPLACED_AND_DISCARDED("대안반영폐기"),
+	SYSTEMATIC_WORDING_REVIEW("체계자구심사"),
+	PLENARY_SUBMITTED("본회의부의안건"),
 	PLENARY_DECIDED("본회의의결"),
 	WITHDRAWN("철회"),
 	GOVERNMENT_TRANSFERRED("정부이송"),
@@ -15,6 +19,15 @@ public enum ProgressStatus {
 	;
 
 	private final String value;
+
+	public static ProgressStatus findByValue(String value) {
+		return Arrays.stream(ProgressStatus.values())
+			.filter(progress -> progress.value.equals(value))
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException(
+				String.format("%s 에 대한 ProgressStatus 은 존재하지 않습니다", value)
+			));
+	}
 
 	ProgressStatus(String value) {
 		this.value = value;
