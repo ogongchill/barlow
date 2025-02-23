83ef3c45234be5649b6f28bb591ae46970bc2aff
chobusiness <nation.normalization@gmail.com>
2025-02-23 16:11:48 +0900

    refactor : 단일 topic 에 대한 request 에서 다양한 topic 에 대한 request 로 리팩토링

--- core/core-notification/src/main/java/com/barlow/notification/DefaultBillNotificationRequest.java
+++ core/core-notification/src/main/java/com/barlow/notification/DefaultBillNotificationRequest.java
@@ -1,43 +1,58 @@
 package com.barlow.notification;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.jetbrains.annotations.NotNull;
 
 /**
  * 법안 알림 : 법안 알림에 관한 기본 전송 알림에 사용한다
- * @param topic 접수의안, 본회의부의안건, 본회의의결, 재의요구, 공포
- * @param representationBill [topic]을 대표하는 하나의 법안 이름
- * @param billInfos [topic] 조건에 해당하는 법안의 정보들 : {법안 ID, 법안 이름}
- * @param totalCount [topic] 조건에 해당하는 법안의 총 개수
+ * @param topicsWithBillInfos {key:법안의 ProgressStatus value:[key]에 해당하는 BillSummary(법안요약정보)}
+ * @see BillSummary
  * @see NotificationType
  */
 public record DefaultBillNotificationRequest(
 	@NotNull NotificationType type,
-	@NotNull String topic,
-	@NotNull String representationBill,
-	@NotNull List<BillInfo> billInfos,
-	@NotNull Integer totalCount
+	@NotNull Map<String, BillSummary> topicsWithBillInfos
 ) implements NotificationRequest {
 
-	public static DefaultBillNotificationRequest of(String topic, List<BillInfo> billInfos) {
-		if (topic == null) {
-			throw new IllegalArgumentException("topic 은 null 이 될 수 없습니다");
+	public static DefaultBillNotificationRequest from(Map<String, List<BillInfo>> topicsWithBillInfos) {
+		if (topicsWithBillInfos == null) {
+			throw new IllegalArgumentException("null 파라미터는 들어올 수 없습니다");
 		}
 		return new DefaultBillNotificationRequest(
 			NotificationType.DEFAULT,
-			topic,
-			billInfos.getFirst().billName(),
-			billInfos,
-			billInfos.size()
+			topicsWithBillInfos.entrySet().stream()
+				.collect(Collectors.toMap(
+					Map.Entry::getKey,
+					entry -> DefaultBillNotificationRequest.BillSummary.from(entry.getValue())
+				))
 		);
 	}
 
 	@Override
 	public List<BillInfo> billInfosByTopic(String topic) {
-		if (!this.topic.equals(topic)) {
-			throw new IllegalArgumentException("잘못된 topic 입니다");
+		return topicsWithBillInfos.get(topic).billInfos;
+	}
+
+	/**
+	 * 법안 요약 정보
+	 * @param billInfos [topic] 조건에 해당하는 법안의 정보들 : {법안 ID, 법안 이름}
+	 * @param representationBill [topic]을 대표하는 하나의 법안 이름
+	 * @param totalCount [topic] 조건에 해당하는 법안의 총 개수
+	 */
+	record BillSummary(
+		@NotNull List<BillInfo> billInfos,
+		@NotNull String representationBill,
+		@NotNull Integer totalCount
+	) {
+		static DefaultBillNotificationRequest.BillSummary from(List<BillInfo> billInfos) {
+			return new DefaultBillNotificationRequest.BillSummary(
+				billInfos,
+				billInfos.getFirst().billName(),
+				billInfos.size()
+			);
 		}
-		return billInfos;
 	}
 }
