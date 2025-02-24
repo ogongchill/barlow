e20266b3df75dffc39bd4e1137f819f359426789
chobusiness <nation.normalization@gmail.com>
2025-02-24 18:50:25 +0900

    chore : 객체 명 수정

--- clients/client-knal-api/src/main/java/com/barlow/client/knal/api/request/BillPreliminaryExaminationInfoRequest.java
+++ clients/client-knal-api/src/main/java/com/barlow/client/knal/api/request/BillPreliminaryExaminationInfoRequest.java
@@ -1,20 +1,21 @@
 package com.barlow.client.knal.api.request;
 
+import com.barlow.client.knal.api.response.BillPreliminaryExaminationInfoResponse;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * <a href="https://www.data.go.kr/data/3037286/openapi.do">IROS4_OA_DV_0401_OpenAPI활용가이드_의안정보(국회사무처)_v1.30<</a></href>의 <code>getBillAdditionalInfo</code>오퍼레이션의 요청 파라미터임
  * @see com.barlow.client.knal.api.Operation
- * @see com.barlow.client.knal.api.response.BillPreLiminaryExaminationInfoResponse
+ * @see BillPreliminaryExaminationInfoResponse
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class BillPreLiminaryExaminationInfoRequest {
+public class BillPreliminaryExaminationInfoRequest {
 
 	@JsonProperty(value = "bill_id")
 	private String billId;
 
-	public BillPreLiminaryExaminationInfoRequest(String billId) {
+	public BillPreliminaryExaminationInfoRequest(String billId) {
 		this.billId = billId;
 	}
 
@@ -30,8 +31,8 @@ public class BillPreLiminaryExaminationInfoRequest {
 			return this;
 		}
 
-		public BillPreLiminaryExaminationInfoRequest build() {
-			return new BillPreLiminaryExaminationInfoRequest(billId);
+		public BillPreliminaryExaminationInfoRequest build() {
+			return new BillPreliminaryExaminationInfoRequest(billId);
 		}
 	}
 
