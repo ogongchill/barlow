5c96669833b3b4adba903b5f73c0289110065317
chobusiness <nation.normalization@gmail.com>
2025-02-26 00:22:49 +0900

    feature : 법안 추적 후 더티체킹 step

---
+++ batch/batch-core/src/main/java/com/barlow/batch/core/tracebill/job/step/TraceBillDirtyCheckTasklet.java
@@ -0,0 +1,59 @@
+package com.barlow.batch.core.tracebill.job.step;
+
+import java.time.LocalDate;
+
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+import com.barlow.batch.core.tracebill.job.NationalAssemblyLegislationClient;
+import com.barlow.batch.core.tracebill.job.PreviousBillBatchRepository;
+import com.barlow.batch.core.tracebill.job.CurrentBillInfoResult;
+import com.barlow.batch.core.tracebill.job.PreviousBills;
+import com.barlow.batch.core.tracebill.job.UpdatedBillShareRepository;
+import com.barlow.batch.core.tracebill.job.UpdatedBills;
+import com.barlow.batch.core.utils.HashUtil;
+
+@Component
+@StepScope
+public class TraceBillDirtyCheckTasklet implements Tasklet {
+
+	private final NationalAssemblyLegislationClient client;
+	private final PreviousBillBatchRepository previousBillBatchRepository;
+	private final UpdatedBillShareRepository billShareRepository;
+
+	public TraceBillDirtyCheckTasklet(
+		NationalAssemblyLegislationClient client,
+		PreviousBillBatchRepository previousBillBatchRepository,
+		UpdatedBillShareRepository billShareRepository
+	) {
+		this.client = client;
+		this.previousBillBatchRepository = previousBillBatchRepository;
+		this.billShareRepository = billShareRepository;
+	}
+
+	@Override
+	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
+		JobExecution jobExecution = contribution.getStepExecution().getJobExecution();
+		JobParameters jobParameters = jobExecution.getJobParameters();
+		LocalDate startProposeDate = jobParameters.getLocalDate("startProposeDate");
+		LocalDate batchDate = jobParameters.getLocalDate("batchDate");
+
+		CurrentBillInfoResult currentBillInfo = client.getTraceBillInfo(startProposeDate, batchDate);
+		PreviousBills previousBills = new PreviousBills(
+			previousBillBatchRepository.findAllPreviousBetween(startProposeDate, batchDate)
+		);
+		UpdatedBills updatedBills = previousBills.dirtyCheck(currentBillInfo);
+
+		String hashKey = HashUtil.generate(updatedBills);
+		jobExecution.getExecutionContext().put("updatedBills", hashKey);
+		billShareRepository.save(hashKey, updatedBills);
+
+		return RepeatStatus.FINISHED;
+	}
+}
