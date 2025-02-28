d5b48dd3dbe594e46d370c653b463127f293d498
chobusiness <nation.normalization@gmail.com>
2025-02-28 18:23:02 +0900

    feature : core auth 모듈 용 error

---
+++ core/core-auth/src/main/java/com/barlow/core/support/error/CoreAuthErrorCode.java
@@ -0,0 +1,10 @@
+package com.barlow.core.support.error;
+
+public enum CoreAuthErrorCode {
+	E500,
+	E400,
+	E401,
+	E404,
+	E409,
+	;
+}
---
+++ core/core-auth/src/main/java/com/barlow/core/support/error/CoreAuthErrorType.java
@@ -0,0 +1,44 @@
+package com.barlow.core.support.error;
+
+import static com.barlow.core.support.error.CoreAuthErrorCode.E400;
+import static com.barlow.core.support.error.CoreAuthErrorCode.E401;
+import static com.barlow.core.support.error.CoreAuthErrorCode.E404;
+import static com.barlow.core.support.error.CoreAuthErrorCode.E409;
+import static com.barlow.core.support.error.CoreAuthErrorCode.E500;
+import static org.springframework.boot.logging.LogLevel.ERROR;
+import static org.springframework.boot.logging.LogLevel.WARN;
+
+import org.springframework.boot.logging.LogLevel;
+import org.springframework.http.HttpStatus;
+
+public enum CoreAuthErrorType {
+
+	DEFAULT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, E500, ERROR),
+	BAD_REQUEST(HttpStatus.BAD_REQUEST, E400, WARN),
+	UNAUTHORIZED(HttpStatus.UNAUTHORIZED, E401, WARN),
+	NOT_FOUND(HttpStatus.NOT_FOUND, E404, WARN),
+	CONFLICT(HttpStatus.CONFLICT, E409, WARN),
+	;
+
+	private final HttpStatus status;
+	private final CoreAuthErrorCode code;
+	private final LogLevel logLevel;
+
+	CoreAuthErrorType(HttpStatus status, CoreAuthErrorCode code, LogLevel logLevel) {
+		this.status = status;
+		this.code = code;
+		this.logLevel = logLevel;
+	}
+
+	public HttpStatus getStatus() {
+		return status;
+	}
+
+	public CoreAuthErrorCode getCode() {
+		return code;
+	}
+
+	public LogLevel getLogLevel() {
+		return logLevel;
+	}
+}
