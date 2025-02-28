2761c404fbd9d7b863d8205d7305a17641dba942
chobusiness <nation.normalization@gmail.com>
2025-02-28 18:32:51 +0900

    feature : core-auth 모듈 정의 filter 추가

---
+++ core/core-api/src/main/java/com/barlow/core/api/config/WebFilterConfiguration.java
@@ -0,0 +1,25 @@
+package com.barlow.core.api.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.barlow.core.auth.filter.InboundJwtAuthenticationFilter;
+
+@Configuration
+public class WebFilterConfiguration {
+
+	private final InboundJwtAuthenticationFilter inboundJwtAuthenticationFilter;
+
+	public WebFilterConfiguration(InboundJwtAuthenticationFilter inboundJwtAuthenticationFilter) {
+		this.inboundJwtAuthenticationFilter = inboundJwtAuthenticationFilter;
+	}
+
+	@Bean
+	public FilterRegistrationBean<InboundJwtAuthenticationFilter> filterRegistrationBean() {
+		FilterRegistrationBean<InboundJwtAuthenticationFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+		filterRegistrationBean.setFilter(inboundJwtAuthenticationFilter);
+		filterRegistrationBean.addUrlPatterns("/*");
+		return filterRegistrationBean;
+	}
+}
