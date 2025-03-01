3cfccf6a8f8b88369faef08e6b997b7ef3b9da62
chobusiness <nation.normalization@gmail.com>
2025-03-01 16:10:58 +0900

    add : DeviceJpaEntity 조회 및 token 업데이트 메서드 추가

--- storage/db-core/src/main/java/com/barlow/storage/db/core/DeviceJpaRepository.java
+++ storage/db-core/src/main/java/com/barlow/storage/db/core/DeviceJpaRepository.java
@@ -1,13 +1,15 @@
 package com.barlow.storage.db.core;
 
-import java.util.List;
-import java.util.Set;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DeviceJpaRepository extends JpaRepository<DeviceJpaEntity, Long> {
 
-	@Query("SELECT d FROM DeviceJpaEntity d WHERE d.status = 'ACTIVE' AND d.memberNo IN :memberNos")
-	List<DeviceJpaEntity> findAllByStatusActiveAndMemberNoIn(Set<Long> memberNos);
+	DeviceJpaEntity findByDeviceId(String deviceId);
+
+	@Modifying
+	@Query("UPDATE DeviceJpaEntity d SET d.token = :deviceToken WHERE d.deviceId = :deviceId")
+	void updateDeviceToken(@Param("deviceId") String deviceId, @Param("deviceToken") String deviceToken);
 }
