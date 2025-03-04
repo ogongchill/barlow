9cd9c6470d81a6b89ed487069aa40fd2b223e5b1
chobusiness <nation.normalization@gmail.com>
2025-03-04 23:50:54 +0900

    add : forbidden 타입 추가

--- core/core-api/src/main/java/com/barlow/core/support/error/CoreApiErrorCode.java
+++ core/core-api/src/main/java/com/barlow/core/support/error/CoreApiErrorCode.java
@@ -4,6 +4,7 @@ public enum CoreApiErrorCode {
 	E500,
 	E400,
 	E401,
+	E403,
 	E404,
 	E409,
 	;
--- core/core-api/src/main/java/com/barlow/core/support/error/CoreApiErrorType.java
+++ core/core-api/src/main/java/com/barlow/core/support/error/CoreApiErrorType.java
@@ -2,6 +2,7 @@ package com.barlow.core.support.error;
 
 import static com.barlow.core.support.error.CoreApiErrorCode.E400;
 import static com.barlow.core.support.error.CoreApiErrorCode.E401;
+import static com.barlow.core.support.error.CoreApiErrorCode.E403;
 import static com.barlow.core.support.error.CoreApiErrorCode.E404;
 import static com.barlow.core.support.error.CoreApiErrorCode.E409;
 import static com.barlow.core.support.error.CoreApiErrorCode.E500;
@@ -17,6 +18,7 @@ public enum CoreApiErrorType {
 	DEFAULT_ERROR(INTERNAL_SERVER_ERROR, E500, "An unexpected error has occurred.", ERROR),
 	BAD_REQUEST(HttpStatus.BAD_REQUEST, E400, "", WARN),
 	UNAUTHORIZED(HttpStatus.UNAUTHORIZED, E401, "", WARN),
+	FORBIDDEN(HttpStatus.FORBIDDEN, E403, "", WARN),
 	NOT_FOUND(HttpStatus.NOT_FOUND, E404, "", WARN),
 	CONFLICT(HttpStatus.CONFLICT, E409, "", WARN),
 	;
