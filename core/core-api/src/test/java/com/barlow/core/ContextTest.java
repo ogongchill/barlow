2931966e2fb7878d8222239d829a22d94046494f
chobusiness <nation.normalization@gmail.com>
2025-03-04 23:54:10 +0900

    test : context 레벨 테스트 추상화

---
+++ core/core-api/src/test/java/com/barlow/core/ContextTest.java
@@ -0,0 +1,11 @@
+package com.barlow.core;
+
+import org.junit.jupiter.api.Tag;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestConstructor;
+
+@Tag("context")
+@SpringBootTest
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+public abstract class ContextTest {
+}
