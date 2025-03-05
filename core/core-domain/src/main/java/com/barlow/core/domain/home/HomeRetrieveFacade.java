02fc88909de35f5500285b4cf0c074b0e7b53541
chobusiness <nation.normalization@gmail.com>
2025-03-05 18:32:50 +0900

    add : 홈에서 [오늘 접수된 법안 썸네일 조회] service 추가

--- core/core-domain/src/main/java/com/barlow/core/domain/home/HomeRetrieveFacade.java
+++ core/core-domain/src/main/java/com/barlow/core/domain/home/HomeRetrieveFacade.java
@@ -1,5 +1,6 @@
 package com.barlow.core.domain.home;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -7,23 +8,32 @@ import org.springframework.stereotype.Service;
 import com.barlow.core.domain.User;
 import com.barlow.core.domain.home.notificationcenter.NotificationCenterItem;
 import com.barlow.core.domain.home.notificationcenter.NotificationCenterItemRetrieveService;
+import com.barlow.core.domain.home.todaybill.TodayBillPostThumbnail;
+import com.barlow.core.domain.home.todaybill.TodayBillPostThumbnailRetrieveService;
 
 @Service
 public class HomeRetrieveFacade {
 
 	private final HomeRetrieveService homeRetrieveService;
+	private final TodayBillPostThumbnailRetrieveService todayBillPostThumbnailRetrieveService;
 	private final NotificationCenterItemRetrieveService notificationCenterItemRetrieveService;
 
 	public HomeRetrieveFacade(
 		HomeRetrieveService homeRetrieveService,
+		TodayBillPostThumbnailRetrieveService todayBillPostThumbnailRetrieveService,
 		NotificationCenterItemRetrieveService notificationCenterItemRetrieveService
 	) {
 		this.homeRetrieveService = homeRetrieveService;
+		this.todayBillPostThumbnailRetrieveService = todayBillPostThumbnailRetrieveService;
 		this.notificationCenterItemRetrieveService = notificationCenterItemRetrieveService;
 	}
 
 	public HomeStatus retrieveHome(User user) {
 		return homeRetrieveService.retrieveHome(user);
+	}
+
+	public List<TodayBillPostThumbnail> retrieveTodayBillPostThumbnail(LocalDate today) {
+		return todayBillPostThumbnailRetrieveService.retrieveTodayAll(today);
 	}
 
 	public List<NotificationCenterItem> retrieveNotificationCenter(User user) {
