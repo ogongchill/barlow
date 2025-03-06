0b098ceaa5fe7a9b65a62e27e016eee74fe991a3
chobusiness <nation.normalization@gmail.com>
2025-03-07 01:30:11 +0900

    feature : 현재(오눌) 입법예고 정보 관리 일급 컬렉션

---
+++ batch/batch-core/src/main/java/com/barlow/batch/core/preannounce/job/CurrentPreAnnounceBills.java
@@ -0,0 +1,20 @@
+package com.barlow.batch.core.preannounce.job;
+
+import java.util.List;
+
+public class CurrentPreAnnounceBills {
+
+	private final List<PreAnnounceBatchEntity> values;
+
+	public CurrentPreAnnounceBills(List<PreAnnounceBatchEntity> values) {
+		this.values = values;
+	}
+
+	public NewPreAnnounceBills dirtyCheck(PreviousPreAnnounceBillIds previousBills) {
+		return new NewPreAnnounceBills(
+			values.stream()
+				.filter(currentBill -> previousBills.containsSameBillId(currentBill.billId()))
+				.toList()
+		);
+	}
+}
