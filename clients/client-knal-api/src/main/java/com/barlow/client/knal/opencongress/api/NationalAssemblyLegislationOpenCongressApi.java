9830f35a2745be4053c55cb6b61995c2b50660b5
chobusiness <nation.normalization@gmail.com>
2025-03-06 16:31:08 +0900

    feature : 열린국회정보 feign client api 진행중인 입법예고 추가

--- clients/client-knal-api/src/main/java/com/barlow/client/knal/opencongress/api/NationalAssemblyLegislationOpenCongressApi.java
+++ clients/client-knal-api/src/main/java/com/barlow/client/knal/opencongress/api/NationalAssemblyLegislationOpenCongressApi.java
@@ -1,10 +1,22 @@
 package com.barlow.client.knal.opencongress.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.barlow.client.knal.opencongress.api.common.DefaultRequest;
+import com.barlow.client.knal.opencongress.api.preannounce.PreAnnouncementResponse;
 
 @FeignClient(
 	value = "${knal.open-congress.api.name}",
 	url = "${knal.open-congress.api.url}",
 	configuration = OpenCongressConfig.class)
 public interface NationalAssemblyLegislationOpenCongressApi {
+
+	@GetMapping(
+		value = Operation.GET_PRE_ANNOUNCEMENT,
+		consumes = MediaType.APPLICATION_JSON_VALUE,
+		produces = MediaType.APPLICATION_JSON_VALUE)
+	PreAnnouncementResponse getPreAnnouncement(@SpringQueryMap DefaultRequest request);
 }
