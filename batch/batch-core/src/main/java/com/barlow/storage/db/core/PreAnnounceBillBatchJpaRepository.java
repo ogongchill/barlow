952fa353a98745fce0aa62a7ef774db9c6603fbc
chobusiness <nation.normalization@gmail.com>
2025-03-07 16:20:51 +0900

    refactor : 오늘날짜 이후에 마감되는 입법예고 법안 select 되도록 변경

--- batch/batch-core/src/main/java/com/barlow/storage/db/core/PreAnnounceBillBatchJpaRepository.java
+++ batch/batch-core/src/main/java/com/barlow/storage/db/core/PreAnnounceBillBatchJpaRepository.java
@@ -1,13 +1,12 @@
 package com.barlow.storage.db.core;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface PreAnnounceBillBatchJpaRepository extends JpaRepository<PreAnnounceBillJpaEntity, String> {
 
-	@Query("SELECT pab FROM PreAnnounceBillJpaEntity pab WHERE pab.progressStatus = :inProgressStatus")
-	List<PreAnnounceBillJpaEntity> findAllInProgress(@Param("inProgressStatus") String inProgressStatus);
+	List<PreAnnounceBillJpaEntity> findAllByDeadlineDateGreaterThanEqual(@Param("today") LocalDateTime today);
 }
