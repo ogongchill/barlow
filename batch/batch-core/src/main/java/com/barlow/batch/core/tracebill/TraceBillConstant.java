f8fad908ca1bbca5d0caa58d9f53654511caf3dd
chobusiness <nation.normalization@gmail.com>
2025-03-14 16:06:28 +0900

    refactor : notify step 트랜잭션 never 설정

--- batch/batch-core/src/main/java/com/barlow/batch/core/tracebill/TraceBillConstant.java
+++ batch/batch-core/src/main/java/com/barlow/batch/core/tracebill/TraceBillConstant.java
@@ -6,9 +6,9 @@ public final class TraceBillConstant {
 	}
 
 	public static final String JOB_NAME = "traceBillJob";
-	public static final String FIRST_STEP_NAME = "traceBillDirtyCheckStep";
-	public static final String SECOND_STEP_NAME = "traceBillUpdateStep";
-	public static final String THIRD_STEP_NAME = "traceBillNotifyStep";
+	public static final String TRACE_BILL_DIRTY_CHECK_STEP = "traceBillDirtyCheckStep";
+	public static final String TRACE_BILL_UPDATE_STEP = "traceBillUpdateStep";
+	public static final String TRACE_BILL_NOTIFY_STEP = "traceBillNotifyStep";
 
 	public static final String TRACKING_END_DATE_JOB_PARAMETER = "yesterdayDate";
 	public static final String TRACKING_START_DATE_JOB_PARAMETER = "startProposeDate";
