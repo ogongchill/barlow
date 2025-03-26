66f19e4abda1bee74120aaf9b42b05c53929e0ec
chobusiness <nation.normalization@gmail.com>
2025-03-26 15:58:11 +0900

    refactor : AWS API GATEWAY 사용 안함

--- batch/batch-core/src/main/java/com/barlow/batch/core/BatchApplicationLambdaHandler.java
+++ batch/batch-core/src/main/java/com/barlow/batch/core/BatchApplicationLambdaHandler.java
@@ -1,26 +1,26 @@
 package com.barlow.batch.core;
 
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
-import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class BatchApplicationLambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
+public class BatchApplicationLambdaHandler implements RequestHandler<HttpApiV2ProxyRequest, AwsProxyResponse> {
 
-	private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+	private static final SpringBootLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler;
 
 	static {
 		try {
-			handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(BatchCoreApplication.class);
+			handler = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(BatchCoreApplication.class);
 		} catch (ContainerInitializationException e) {
 			throw new RuntimeException("Unable To Start BatchCoreApplication", e);
 		}
 	}
 
 	@Override
-	public AwsProxyResponse handleRequest(AwsProxyRequest awsProxyRequest, Context context) {
+	public AwsProxyResponse handleRequest(HttpApiV2ProxyRequest awsProxyRequest, Context context) {
 		return handler.proxy(awsProxyRequest, context);
 	}
 }
