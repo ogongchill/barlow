12a2bcd7e06fda63dc350f566bb7d187cb3d2c4a
chobusiness <nation.normalization@gmail.com>
2025-04-24 18:36:33 +0900

    refactor : 요청 시나리오 리팩토링

--- k6/scenarios/committee-subscribe.test.js
+++ k6/scenarios/committee-subscribe.test.js
@@ -7,31 +7,81 @@ const [secret] = new SharedArray('secret', () => {
 });
 const BASE_URL = secret.baseUrl;
 const SLEEP_DURATION = 0.1;
+const LEGISLATION_TYPE = "LEGISLATION_AND_JUDICIARY";
+const VUS = 100;
 
 export const options = {
     scenarios: {
         concurrent_test: {
             executor: 'constant-vus',
-            vus: 100,
-            duration: '30s',
+            vus: VUS,
+            duration: '10s',
         },
     },
 };
 
-export default function () {
-
+export function setup() {
+    return signup();
 }
 
-function subscribe() {
-    const url = `${BASE_URL}/api/v1/legislation-accounts/{legislationType}/subscribe/active`;
-    const res = http.post(url);
+async function signup() {
+    const params = {headers: {"Content-Type": "application/json", "Accept": "application/json"}};
+    const tokens = [];
+    for (let i = 0; i < VUS; i++) {
+        const body = {
+            "deviceOs": "ios",
+            "deviceId": `k6-test-device-id-${i}`,
+            "deviceToken": `k6-test-device-token-${i}`,
+            "nickname": `k6-test-user-nickname-${i}`
+        }
+        const res = await http.asyncRequest(
+            'POST', `${BASE_URL}/api/v1/auth/guest/signup`, JSON.stringify(body), params);
+        tokens.push(res.json('data').accessToken);
+    }
+    return {tokens};
+}
+
+export default function (data) {
+    const token = data.tokens[__VU - 1];
+    const params = getRequestParams(token);
+    retrieveCommittee(params);
+    if (__VU <= VUS / 2) {
+        subscribe(params);
+    } else {
+        unsubscribe(params);
+    }
+}
+
+function retrieveCommittee(params) {
+    const url = `${BASE_URL}/api/v1/legislation-accounts/committees/info`;
+    const res = http.get(url, params)
     check(res, {"status is 200": (res) => res.status === 200});
     sleep(SLEEP_DURATION);
 }
 
-function unsubscribe() {
-    const url = `${BASE_URL}/api/v1/legislation-accounts/{legislationType}/subscribe/deactive`;
-    const res = http.post(url);
+function subscribe(params) {
+    const url = `${BASE_URL}/api/v1/legislation-accounts/${LEGISLATION_TYPE}/subscribe/activate`;
+    const res = http.post(url, params);
     check(res, {"status is 200": (res) => res.status === 200});
     sleep(SLEEP_DURATION);
+}
+
+function unsubscribe(params) {
+    const url = `${BASE_URL}/api/v1/legislation-accounts/${LEGISLATION_TYPE}/subscribe/deactivate`;
+    const res = http.post(url, params);
+    check(res, {"status is 200": (res) => res.status === 200});
+    sleep(SLEEP_DURATION);
+}
+
+function getRequestParams(token) {
+    return {
+        headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            'X-Client-OS': secret.clientOs,
+            'X-Client-OS-Version': secret.clientOsVersion,
+            'X-Device-ID': secret.deviceId,
+        }
+    };
 }
