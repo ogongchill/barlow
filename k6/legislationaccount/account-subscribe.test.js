80bfc2375312447ac5aedcbbe739bcde206ed033
chobusiness <nation.normalization@gmail.com>
2025-04-24 21:04:01 +0900

    hotfix : account subscribe 단독 테스트로 변경

--- k6/legislationaccount/account-subscribe.test.js
+++ k6/legislationaccount/account-subscribe.test.js
@@ -11,13 +11,9 @@ const LEGISLATION_TYPE = "LEGISLATION_AND_JUDICIARY";
 const VUS = 100;
 
 export const options = {
-    scenarios: {
-        concurrent_test: {
-            executor: 'constant-vus',
-            vus: VUS,
-            duration: '10s',
-        },
-    },
+    stages: [
+        {duration: '10s', target: VUS}
+    ]
 };
 
 export function setup() {
@@ -41,28 +37,14 @@ async function signup() {
     return {tokens};
 }
 
-export default function (data) {
-    const token = data.tokens[__VU - 1];
-    const params = getRequestParams(token);
-    subscribe(params);
+export default function ({tokens}) {
+    const token = tokens[__VU - 1];
+    subscribe(token);
 }
 
-function retrieveCommittee(params) {
-    const url = `${BASE_URL}/api/v1/legislation-accounts/committees/info`;
-    const res = http.get(url, params)
-    check(res, {"status is 200": (res) => res.status === 200});
-    sleep(SLEEP_DURATION);
-}
-
-function subscribe(params) {
+function subscribe(token) {
     const url = `${BASE_URL}/api/v1/legislation-accounts/${LEGISLATION_TYPE}/subscribe/activate`;
-    const res = http.post(url, params);
-    check(res, {"status is 200": (res) => res.status === 200});
-    sleep(SLEEP_DURATION);
-}
-
-function getRequestParams(token) {
-    return {
+    const params = {
         headers: {
             Authorization: `Bearer ${token}`,
             'Content-Type': 'application/json',
@@ -72,4 +54,7 @@ function getRequestParams(token) {
             'X-Device-ID': secret.deviceId,
         }
     };
+    const res = http.post(url, null, params);
+    check(res, {"status is 200": (res) => res.status === 200});
+    sleep(SLEEP_DURATION);
 }
