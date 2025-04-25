40571fb14eb662820195b55263d0fef0c2ec3228
chobusiness <nation.normalization@gmail.com>
2025-04-25 13:35:11 +0900

    feature : batch lambda 스케줄러 컴포넌트

---
+++ core/core-api/src/main/java/com/barlow/core/scheduler/BatchLambdaScheduler.java
@@ -0,0 +1,53 @@
+package com.barlow.core.scheduler;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class BatchLambdaScheduler {
+
+	private final RestTemplate restTemplate;
+	private final String preAnnounceBatchFuncUrl;
+	private final String trackingBillBatchFuncUrl;
+	private final String updateBillBatchFuncUrl;
+
+	public BatchLambdaScheduler(
+		@Value("${scheduler.batch-lambda.url.pre-announce}") String preAnnounceBatchFuncUrl,
+		@Value("${scheduler.batch-lambda.url.tracking-bill}") String trackingBillBatchFuncUrl,
+		@Value("${scheduler.batch-lambda.url.today-bill}") String updateBillBatchFuncUrl,
+		RestTemplate restTemplate
+	) {
+		this.preAnnounceBatchFuncUrl = preAnnounceBatchFuncUrl;
+		this.trackingBillBatchFuncUrl = trackingBillBatchFuncUrl;
+		this.updateBillBatchFuncUrl = updateBillBatchFuncUrl;
+		this.restTemplate = restTemplate;
+	}
+
+	@Scheduled(cron = "0 0 12 * * 1-5")
+	public void scheduledTrackingPreAnnouncementBillBatch() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+		restTemplate.exchange(preAnnounceBatchFuncUrl, HttpMethod.POST, new HttpEntity<>(headers), Void.class);
+	}
+
+	@Scheduled(cron = "0 30 15 * * 1-5")
+	public void scheduledTrackingBillInfoBatch() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+		restTemplate.exchange(trackingBillBatchFuncUrl, HttpMethod.POST, new HttpEntity<>(headers), Void.class);
+	}
+
+	@Scheduled(cron = "0 0 19 * * 1-5")
+	public void scheduledUpdateTodayBillBatch() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+		restTemplate.exchange(updateBillBatchFuncUrl, HttpMethod.POST, new HttpEntity<>(headers), Void.class);
+	}
+}
