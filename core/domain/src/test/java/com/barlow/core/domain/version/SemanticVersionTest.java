cd56e670885407cedc790559945fbff74f6b9707
Sy P <yongsa0221@gmail.com>
2025-05-27 18:58:27 +0900

    feature: 버전 정보 jpa entity및 repository추가
    + batch 설정 파일 누락에 따른 테스트 코드실행 불가

--- core/domain/src/test/java/com/barlow/core/domain/version/SemanticVersionTest.java
+++ core/domain/src/test/java/com/barlow/core/domain/version/SemanticVersionTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class SemanticVersionTest {
 
