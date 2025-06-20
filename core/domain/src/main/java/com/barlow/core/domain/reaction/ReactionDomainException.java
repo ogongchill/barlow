77b7e710136887fda2343a5a4e55b56ee050f2ce
chobusiness <nation.normalization@gmail.com>
2025-06-20 21:08:24 +0900

    feature : reaction 도메인 예외

---
+++ core/domain/src/main/java/com/barlow/core/domain/reaction/ReactionDomainException.java
@@ -0,0 +1,24 @@
+package com.barlow.core.domain.reaction;
+
+import com.barlow.core.exception.CoreDomainException;
+import com.barlow.core.exception.CoreDomainExceptionCode;
+import com.barlow.core.exception.CoreDomainExceptionLevel;
+import com.barlow.core.exception.CoreDomainExceptionType;
+
+public class ReactionDomainException extends CoreDomainException {
+
+	protected ReactionDomainException(CoreDomainExceptionCode code, CoreDomainExceptionLevel level, String message) {
+		super(code, level, message);
+	}
+
+	protected ReactionDomainException(CoreDomainExceptionType exceptionType, String message) {
+		super(exceptionType, message);
+	}
+
+	static ReactionDomainException alreadyReact(String targetType, String targetId) {
+		return new ReactionDomainException(
+			CoreDomainExceptionType.CONFLICT_EXCEPTION,
+			String.format("이미 리액션한 %s - $s 입니다. 리액션은 한 종류만 가능합니다", targetType, targetId)
+		);
+	}
+}
