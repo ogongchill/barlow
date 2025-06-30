f7602634c6780a13fec795a5e60632791f7b22be
chobusiness <nation.normalization@gmail.com>
2025-06-30 16:37:11 +0900

    refactor : 전송 실패/성공 결과 포함 및 error code 로 실패 메시지 그루핑 메서드 추가

--- services/notification/src/main/java/com/barlow/services/notification/worker/NotificationResult.java
+++ services/notification/src/main/java/com/barlow/services/notification/worker/NotificationResult.java
@@ -2,17 +2,19 @@ package com.barlow.services.notification.worker;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.MessagingErrorCode;
 
 public record NotificationResult(
-	Map<Message, MessagingErrorCode> result
+	Map<NotificationMessage, MessagingErrorCode> failureResult,
+	List<NotificationMessage> successResult
 ) {
 	boolean hasRetryableFailure() {
-		return result.entrySet()
-			.stream()
-			.filter(entry -> entry.getValue().equals(MessagingErrorCode.INTERNAL)
+		return failureResult.entrySet().stream()
+			.filter(entry
+				-> entry.getValue().equals(MessagingErrorCode.INTERNAL)
 				|| entry.getValue().equals(MessagingErrorCode.UNAVAILABLE)
 				|| entry.getValue().equals(MessagingErrorCode.QUOTA_EXCEEDED))
 			.map(Map.Entry::getKey)
@@ -20,23 +22,42 @@ public record NotificationResult(
 			.isPresent();
 	}
 
-	List<Message> getRetryableMessages() {
-		return result.entrySet()
-			.stream()
-			.filter(entry -> entry.getValue().equals(MessagingErrorCode.INTERNAL)
+	public List<NotificationMessage> getRetryableMessages() {
+		return failureResult.entrySet().stream()
+			.filter(entry
+				-> entry.getValue().equals(MessagingErrorCode.INTERNAL)
 				|| entry.getValue().equals(MessagingErrorCode.UNAVAILABLE)
 				|| entry.getValue().equals(MessagingErrorCode.QUOTA_EXCEEDED))
 			.map(Map.Entry::getKey)
 			.toList();
 	}
 
-	List<Message> getNonRetryableMessages() {
-		return result.entrySet()
-			.stream()
-			.filter(entry -> !entry.getValue().equals(MessagingErrorCode.INTERNAL)
+	List<NotificationMessage> getNonRetryableMessages() {
+		return failureResult.entrySet().stream()
+			.filter(entry
+				-> !entry.getValue().equals(MessagingErrorCode.INTERNAL)
 				&& !entry.getValue().equals(MessagingErrorCode.UNAVAILABLE)
 				&& !entry.getValue().equals(MessagingErrorCode.QUOTA_EXCEEDED))
 			.map(Map.Entry::getKey)
 			.toList();
+	}
+
+	public Map<MessagingErrorCode, List<NotificationMessage>> groupRetryableFailuresByError() {
+		return failureResult.entrySet().stream()
+			.filter(entry
+				-> entry.getValue().equals(MessagingErrorCode.INTERNAL)
+				|| entry.getValue().equals(MessagingErrorCode.UNAVAILABLE)
+				|| entry.getValue().equals(MessagingErrorCode.QUOTA_EXCEEDED))
+			.collect(Collectors.groupingBy(
+				Map.Entry::getValue,
+				Collectors.flatMapping(
+					entry -> Stream.of(entry.getKey()),
+					Collectors.toList()
+				)
+			));
+	}
+
+	public List<NotificationMessage> getSuccessMessages() {
+		return successResult;
 	}
 }
