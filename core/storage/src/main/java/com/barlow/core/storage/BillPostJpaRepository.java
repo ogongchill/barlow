e1e780374ac33ff56529b6dafaa017a766badba8
chobusiness <nation.normalization@gmail.com>
2025-07-15 16:02:25 +0900

    refactor : services:post-view 모듈에 있던 update query 로직 core:storage 모듈로 옮김

--- core/storage/src/main/java/com/barlow/core/storage/BillPostJpaRepository.java
+++ core/storage/src/main/java/com/barlow/core/storage/BillPostJpaRepository.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BillPostJpaRepository
 	extends JpaRepository<BillPostJpaEntity, Long>,
@@ -13,4 +16,8 @@ public interface BillPostJpaRepository
 	BillPostJpaEntity findByBillId(String billId);
 
 	List<BillPostJpaEntity> findAllByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
+
+	@Modifying
+	@Query("UPDATE BillPostJpaEntity bp SET bp.viewCount = bp.viewCount + 1 WHERE bp.billId = :billId")
+	void updateViewCount(@Param("billId") String billId);
 }
