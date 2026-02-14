348bee8fecbbf6558af7808976565a214cf73132
Sy P <yongsa0221@gmail.com>
2026-02-15 04:43:02 +0900

    chore: staging 환경 build 설정 추가
    - buildspec-staging.yml 추가
    - staging profile에서 cron schedular 구성하지 않도록 변경

--- app/api/src/main/java/com/barlow/app/scheduler/BatchLambdaScheduler.java
+++ app/api/src/main/java/com/barlow/app/scheduler/BatchLambdaScheduler.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+@Profile("!staging")
 @Component
 public class BatchLambdaScheduler {
 
